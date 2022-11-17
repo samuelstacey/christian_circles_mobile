cab3dc30d25f1b6cb16bc643efc1f0948e0faeb0
Samuel Stacey <ss3467@bath.ac.uk>
2022-11-17 17:17:09 +0000

    CC-46/38 - Setup of flutter contacting azure to acquire a token - IT CONNECTS SUCCESSFULLY

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,118 @@
-import 'package:christian_circles_mobile/pages/about.dart';
-import 'package:christian_circles_mobile/pages/home.dart';
-import 'package:christian_circles_mobile/pages/profile.dart';
-import 'package:christian_circles_mobile/pages/setttings.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:page_transition/page_transition.dart';
+
+import 'package:flutter_azure_b2c/flutter_azure_b2c.dart';
+import 'package:flutter_azure_b2c/B2COperationResult.dart';
+import 'package:flutter_azure_b2c/B2CConfiguration.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  static const String _title = 'Christian Circles';
+class _MyAppState extends State<MyApp> {
+  String _retdata = "";
+  List<String>? _subjects;
+  B2CConfiguration? _configuration;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // It is possible to register callbacks in order to handle return values
+    // from asynchronous calls to the plugin
+    AzureB2C.registerCallback(B2COperationSource.INIT, (result) async {
+      if (result.reason == B2COperationState.SUCCESS) {
+        _configuration = await AzureB2C.getConfiguration();
+      }
+    });
+
+    // Important: Remember to handle redirect states (if you want to support
+    // the web platform with redirect method) and init the AzureB2C plugin
+    // before the material app starts.
+    AzureB2C.handleRedirectFuture().then((_) => AzureB2C.init("b2c_config_template"));
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: _title,
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Azure AD B2C Plugin example app'),
         ),
-        initialRoute: '/',
-        routes: {
-          '/': (context) => const HomePage(),
-          '/about': (context) => const AboutPage(),
-          '/profile': (context) => const ProfilePage(),
-        },
-      onGenerateRoute: (settings) {
-        switch (settings.name) {
-          case '/settings':
-            return PageTransition(
-              child: const SettingsPage(),
-              type: PageTransitionType.scale,
-              alignment: Alignment.topRight,
-              settings: settings,
-            );
-          default:
-            return null;
-        }
-      },
+        body: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    TextButton(
+                        onPressed: () async {
+                          // you can just perform calls to the AzureB2C plugin to
+                          // handle the B2C protocol (e.g. acquire, refresh tokens
+                          // or sign out).
+                          var data = await AzureB2C.policyTriggerInteractive(
+                              _configuration!.defaultAuthority.policyName,
+                              _configuration!.defaultScopes!,
+                              null);
+                          setState(() {
+                            _retdata = data;
+                          });
+                        },
+                        child: Text("LogIn")),
+                    TextButton(
+                        onPressed: () async {
+                          var subjects = await AzureB2C.getSubjects();
+                          var info = await AzureB2C.getUserInfo(subjects![0]);
+                          setState(() {
+                            _subjects = subjects;
+                            _retdata = json.encode(info);
+                          });
+                        },
+                        child: Text("UserInfo")),
+                  ],
+                ),
+                Row(
+                  children: [
+                    TextButton(
+                        onPressed: () async {
+                          var token = await AzureB2C.getAccessToken(_subjects![0]);
+                          setState(() {
+                            _retdata = json.encode(token);
+                          });
+                        },
+                        child: Text("AccessToken")),
+                    TextButton(
+                        onPressed: () async {
+                          var data = await AzureB2C.policyTriggerSilently(
+                            _subjects![0],
+                            _configuration!.defaultAuthority.policyName,
+                            _configuration!.defaultScopes!,
+                          );
+                          setState(() {
+                            _retdata = data;
+                          });
+                        },
+                        child: Text("Refresh")),
+                    TextButton(
+                        onPressed: () async {
+                          var data = await AzureB2C.signOut(_subjects![0]);
+                          setState(() {
+                            _retdata = data;
+                          });
+                        },
+                        child: Text("LogOut")),
+                  ],
+                ),
+                Text(_retdata)
+              ],
+            )),
+      ),
     );
   }
 }
---
+++ lib/other.dart
@@ -0,0 +1,46 @@
+// import 'package:christian_circles_mobile/pages/about.dart';
+// import 'package:christian_circles_mobile/pages/home.dart';
+// import 'package:christian_circles_mobile/pages/profile.dart';
+// import 'package:christian_circles_mobile/pages/setttings.dart';
+// import 'package:flutter/material.dart';
+// import 'package:page_transition/page_transition.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   static const String _title = 'Christian Circles';
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     // return MaterialApp(
+//     //     debugShowCheckedModeBanner: false,
+//     //     title: _title,
+//     //     theme: ThemeData(
+//     //       primarySwatch: Colors.purple,
+//     //     ),
+//     //     initialRoute: '/',
+//     //     routes: {
+//     //       '/': (context) => const HomePage(),
+//     //       '/about': (context) => const AboutPage(),
+//     //       '/profile': (context) => const ProfilePage(),
+//     //     },
+//     //   onGenerateRoute: (settings) {
+//     //     switch (settings.name) {
+//     //       case '/settings':
+//     //         return PageTransition(
+//     //           child: const SettingsPage(),
+//     //           type: PageTransitionType.scale,
+//     //           alignment: Alignment.topRight,
+//     //           settings: settings,
+//     //         );
+//     //       default:
+//     //         return null;
+//     //     }
+//     //   },
+//     // );
+//   }
+// }
